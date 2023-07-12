f7453ec19d0784d82f3763214c99040cd4caa808
Sarthak Singh <105309861+sarthakk1890@users.noreply.github.com>
2023-07-12 20:31:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,6 @@ app.use(fileUpload({
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.json({ limit: '10mb' }));
-// app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use('/api/auth', require('./routes/auth.js'));
 app.use('/api/history', require('./routes/previous.js'));
